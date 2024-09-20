5c98fc1dc641b3c4005a6fb22e725ae2612f851a
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-09-20 10:28:08 +0300

    +

--- Files/Program.cs
+++ Files/Program.cs
@@ -1,10 +1,12 @@
-﻿using System;
+﻿//#define WRITE_TO_FILE
+#define READ_FROM_FILE
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
-using System.Diagnostics;
+using System.Diagnostics;// сам открывает фаил
 
 namespace Files
 {
@@ -12,10 +14,24 @@ namespace Files
     {
         static void Main(string[] args)
         {
+#if WRITE_TO_FILE //Пишем в файл
             StreamWriter sw = new StreamWriter("File.txt");
-            sw.WriteLine("Hello files");
-            sw.Close();
-            Process.Start("notepad", "File.txt");
+            sw.WriteLine("Hello files"); // пишем в поток
+            sw.Close(); // После записи в поток его обязательно нужно закрыть
+            Process.Start("notepad", "File.txt");// сам открывает фаил  
+#endif
+
+#if READ_FROM_FILE // Читаем из файла
+            StreamReader sr = new StreamReader("File.txt");
+
+            while (!sr.EndOfStream) 
+            {
+                string buffer = sr.ReadLine();
+                Console.WriteLine(buffer);
+            }
+
+            sr.Close();
+#endif
         }
     }
 }
