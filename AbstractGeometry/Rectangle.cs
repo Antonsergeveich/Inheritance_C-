92ba4fbe0232071ee6da05994c27b99b58c1e900
Persh_vx33@top-academy.ru <Persh_vx33@top-academy.ru>
2024-09-23 10:17:16 +0300

    +

---
+++ AbstractGeometry/Rectangle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AbstractGeometry
+{
+    internal class Rectangle : Shape
+    {
+        double width;
+        double height;
+
+        public double Width
+        {
+            get { return width; }
+            set { width = value; }
+        }
+        public double Height
+        {
+            get { return height; }
+            set { height = value; }
+        }
+        public Rectangle
+            (
+            double width, double height,
+            uint start_x, uint start_y, uint line_width
+            ) : base(start_x, start_y, line_width, color)
+        {
+            Width = width;
+            Height = height;
+        }
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+        public override double GetPerimeter()
+        {
+            return (Width + Height) * 2;
+        }
+    }
+}
---
+++ AbstractGeometry/Shape.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace AbstractGeometry
+{
+    abstract class Shape //Форма
+    {
+        uint start_x;
+        uint start_y;
+        uint line_width;
+        public Color Color { get; set; }
+        //https://learn.microsoft.com/ru-ru/dotnet/api/system.drawing.color?view=net-7.0
+        public uint StartX
+        {
+            get { return start_x; }
+            set { start_x = value; }
+        }
+        public uint StartY
+        {
+            get { return start_y; }
+            set { start_y = value; }
+        }
+        public uint LineWidth
+        {
+            get { return line_width; }
+            set { line_width = value; }
+        }
+        public Shape(uint start_x, uint start_y, uint line_width, Color color)
+        {
+            StartX = start_x;
+            StartY = start_y;
+            LineWidth = line_width;
+            Color = color;
+        }
+        public abstract double GetArea();
+        public abstract double GetPerimeter();
+
+
+    }
+}
